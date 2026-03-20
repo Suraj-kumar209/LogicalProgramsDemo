b2bafd8c63a9867e812eb2e3f5948d4c4c51995e
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-20 16:43:38 +0530

    code update

--- LogicalProgramming/obj/Debug/net8.0/LogicalProgramming.AssemblyInfo.cs
+++ LogicalProgramming/obj/Debug/net8.0/LogicalProgramming.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicalProgramming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53655d54ff2a3823b7dcab01c5ee1144523b031a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicalProgramming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicalProgramming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
