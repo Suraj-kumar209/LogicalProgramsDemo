61fa54b246c6e23234e4bbe9f5d18886224d4111
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-24 18:43:21 +0530

    Roman number program.

---
+++ LogicalProgramming/NumberPyramidPattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class NumberPyramidPattern
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of rows:");
+            int n= Convert.ToInt32(Console.ReadLine());
+            for (int i=n;i>=0;i--)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                if (i > 0)
+                {
+                    Console.WriteLine();
+                }
+
+            }
+            for (int i=1;i<=n;i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
