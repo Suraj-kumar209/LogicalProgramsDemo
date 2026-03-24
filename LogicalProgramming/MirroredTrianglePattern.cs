9a4184c5b952e570fbe11558c9f2a0a4078a8c15
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-24 17:41:36 +0530

    triangle pattern programs.

---
+++ LogicalProgramming/MirroredTrianglePattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class MirroredTrianglePattern
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number of rows");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i= 1;i<=n;i++)
+            {
+                Console.Write(new string('*',i));
+                Console.Write(new string(' ',n-i));
+                Console.WriteLine(new string('*',n-i+1));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalProgramming/RightTrianglePattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class RightTrianglePattern
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number of rows");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<n;i++)
+            {
+                for (int j=1;j<=i;j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for(int k=n-2; k>0; k--)
+            {
+                for (int l=1;l<=k;l++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
