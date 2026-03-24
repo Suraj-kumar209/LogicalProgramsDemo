ba36759c36b5cc8c0fa0ca844402da4fce002e0d
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-24 18:50:34 +0530

    Number Triangle program.

---
+++ LogicalProgramming/NumberTriangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class NumberTriangle
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of rows:");
+            int n= Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=n;i++)
+            {
+                for(int j = 1; j <=i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
