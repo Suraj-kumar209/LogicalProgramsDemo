1f4d57433c3e73f40a6006ee1f1d3558368f8da9
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-26 14:47:27 +0530

    summary

---
+++ LogicalProgramming/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class SelectionSort
+    {
+        static void Main()
+        {
+            int[] arr = { 59, 29, 7, 43, 92, 4,38 };
+            for(int i=0;i<arr.Length-1; i++)
+            {
+                int min = i;
+                for(int j=i+1;j<arr.Length; j++)
+                {
+                    if (arr[min] > arr[j])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    int lowerValue = arr[min];
+                    arr[min] = arr[i];
+                    arr[i] = lowerValue;
+                }
+            }
+            foreach(int i in arr)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
