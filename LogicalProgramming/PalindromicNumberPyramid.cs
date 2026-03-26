09189f4662d53e569fcfcc8848b428756dfc3169
Suraj-kumar209 <chaubeysuraj425@gmail.com>
2026-03-26 16:22:05 +0530

    Pattern programs.

---
+++ LogicalProgramming/PalindromicNumberPyramid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PalindromicNumberPyramid
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of rows:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            for (int i=1;i<=n;i++)
+            {
+                for (int space=1;space<=(n-i);space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j=1;j<=i;j++)
+                {
+                    Console.Write(j);
+                }
+                for (int k=(i-1);k>=1;k--)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalProgramming/PascalsNumberTriangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PascalsNumberTriangle
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of rows:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            int result;
+            for (int i=0;i<=n;i++)
+            {
+                result = 1;
+                for (int j=i;j<=n-1;j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k=0;k<=i;k++)
+                {
+                    Console.Write(result+" ");
+                    result = (result*(i-k)/(k+1));
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
